d7bc4aca5c4ea1e9703d0119c68a53e639bc6184
Paula Rodriguez <Papita@Jozsefs-MacBook-Pro.local>
2022-04-11 13:41:52 +0100

    web tech quiz

---
+++ data/data.js
@@ -0,0 +1,56 @@
+export const data = [
+
+{
+    question: 'These three simple ingredients make one of the healthiest and tastiest salads. What do you get with basil, tomato, and mozzarella?',
+    choices: ['Greek salad', 'Caprese salad', 'Summer salad'],
+    answer: 'Caprese salad',
+},
+
+{
+    question: 'A balanced breakfast is the key to a good day! What is made from an English muffin, poached egg, bacon, and Hollandaise sauce?',
+    choices: ['Burguer', 'Eggs benedict', 'Scottish breakfast'],
+    answer: 'Eggs benedict',
+},
+
+{
+    question: 'This meal can come with many more ingredients.But at its simplest, it includes a large tortilla, cheese, salsa, and maybe some chichen.',
+    choices: ['Burritos', 'Wrap', 'Quesadillas'],
+    answer: 'Quesadillas',
+},
+
+{
+    question: 'This tasty breakfast has the simplest ingridients. What can you make with these four items?',
+    choices: ['French toast', 'Pasta', 'Soup'],
+    answer: 'French toast',
+},
+
+{
+    question: 'This italian appetizer is made of the simplest ingredients: Toasted bread, chopped tomato, garlic, olive oil, and basil',
+    choices: ['Bruschetta', 'Pizza', 'Focaccia'],
+    answer: 'Bruschetta',
+},
+
+{
+    question: "It's snack time! You've got a tomato, avocado, cilantro, garlic, lime, onion, and salt and pepper. What are you going to make?",
+    choices: ['Greek salad', 'Guacamole','Mexican sauce'],
+    answer: 'Guacamole',
+},
+
+{
+    question: 'Very popular snack made from fried dough pastry, cut into sausage shapes and doused in sugar. They are a favourite at fiestas, or street parties, when they are sold by roadside vendors. Dipping them in hot melted chocolate is pretty much the law. ',
+    choices: ['German sausages','Churros','Mozzarella sticks'],
+    answer: 'Churros',
+},
+
+{
+    question: 'You have these ingredients for Saturday night supper! What will you make with ground beef, beans, chopped onion, tomato sauce, and a whole lot of spices?',
+    choices: ['Chilli con carne', 'Bolognese', 'Tomato soup'],
+    answer: 'Chilli con carne',
+},
+
+{
+    question: 'This is a baked custard dessert topped with a melted sugar crust. With a velvety and pudding-like texture, this burnt cream dessert is so dreamy!',
+    choices: ['Tiramisu', 'Creme Brulee', 'Apple Pie'],
+    answer: 'Creme Brulee',
+},
+]; 
