698a1a38b5e2d1159e12b07ed9652937752cc8f3
Paula Rodriguez <Papita@Jozsefs-MacBook-Pro.local>
2022-04-12 16:39:46 +0100

    questions amended

--- data/data.js
+++ data/data.js
@@ -1,13 +1,12 @@
 export const data = [
 
 {
-    image: "salad.png", 
     question: 'These three simple ingredients make one of the healthiest and tastiest salads. What do you get with basil, tomato, and mozzarella?',
     choices: ['Greek salad', 'Caprese salad', 'Summer salad'],
     answer: 'Caprese salad',
 },
 
-{   image: "salad.png",
+{   
     question: 'A balanced breakfast is the key to a good day! What is made from an English muffin, poached egg, bacon, and Hollandaise sauce?',
     choices: ['Burguer', 'Eggs benedict', 'Scottish breakfast'],
     answer: 'Eggs benedict',
@@ -19,43 +18,43 @@ export const data = [
     answer: 'Quesadillas',
 },
 
-{   image: "salad.png",
-    question: 'This tasty breakfast has the simplest ingridients. What can you make with these four items?',
+{ 
+    question: 'This tasty breakfast has the simplest ingridients. This dish made of sliced bread soaked in beaten eggs and typically milk, then pan fried. An alternative name is Eggy bread.',
     choices: ['French toast', 'Pasta', 'Soup'],
     answer: 'French toast',
 },
 
-{   image: "salad.png",
+{  
     question: 'This italian appetizer is made of the simplest ingredients: Toasted bread, chopped tomato, garlic, olive oil, and basil',
     choices: ['Bruschetta', 'Pizza', 'Focaccia'],
     answer: 'Bruschetta',
 },
 
-{   image: "salad.png",
+{   
     question: "It's snack time! You've got a tomato, avocado, cilantro, garlic, lime, onion, and salt and pepper. What are you going to make?",
     choices: ['Greek salad', 'Guacamole','Mexican sauce'],
     answer: 'Guacamole',
 },
 
-{   image: "salad.png",
+{   
     question: 'Very popular snack made from fried dough pastry, cut into sausage shapes and doused in sugar. They are a favourite at fiestas, or street parties, when they are sold by roadside vendors. Dipping them in hot melted chocolate is pretty much the law. ',
     choices: ['German sausages','Churros','Mozzarella sticks'],
     answer: 'Churros',
 },
 
-{   image: "salad.png",
+{   
     question: 'You have these ingredients for Saturday night supper! What will you make with ground beef, beans, chopped onion, tomato sauce, and a whole lot of spices?',
     choices: ['Chilli con carne', 'Bolognese', 'Tomato soup'],
     answer: 'Chilli con carne',
 },
 
-{   image: "salad.png",
+{   
     question: 'This is a baked custard dessert topped with a melted sugar crust. With a velvety and pudding-like texture, this burnt cream dessert is so dreamy!',
     choices: ['Tiramisu', 'Creme Brulee', 'Apple Pie'],
     answer: 'Creme Brulee',
 },
 
-{   image: "salad.png",
+{   
     question: 'This is a traditional Scottish pudding, similar to a trifle. It is layered dessert, consisting of whipped cream, toasted oats, and juicy raspberries. Of course, it would not be scottish without a touch of whiskey in the mix!',
     choices: ['Scones', 'Cranachan', 'Shortbread'],
     answer: 'Cranachan',
